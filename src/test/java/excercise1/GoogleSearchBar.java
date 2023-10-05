e644ce91473b95d2ebf642640ce53efdb4f60962
nahueld97 <nahueld97@outlook.com>
2023-10-04 21:35:22 -0300

    tested and modified

--- src/test/java/excercise1/GoogleSearchBar.java
+++ src/test/java/excercise1/GoogleSearchBar.java
@@ -6,6 +6,7 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
 
 import java.time.Duration;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -29,16 +30,18 @@ public class GoogleSearchBar {
 
 	@Test
 	public void f() {
-		wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		
 		WebElement searchBarElement = driver.findElement(searchBar);
 		searchBarElement.sendKeys("auto");
 		wait.until(ExpectedConditions.visibilityOfElementLocated(resultsList));
 		WebElement resultsElement = driver.findElements(resultsList).get(0);
 		List<WebElement> suggestionsAuto = resultsElement.findElements(By.cssSelector("[role=\"option\"]"));
+		List<String> suggestionsAutoStrings = new LinkedList<String>();
 		System.out.println("primera busqueda");
 		for (WebElement webElement : suggestionsAuto) {
 			System.out.println("-"+webElement.getAttribute("aria-label"));
+			suggestionsAutoStrings.add(webElement.getAttribute("aria-label"));
 		}
 		
 		searchBarElement.clear();
@@ -54,24 +57,30 @@ public class GoogleSearchBar {
 		resultsElement = driver.findElements(resultsList).get(0);
 		System.out.println("segunda busqueda");
 		List<WebElement> suggestionsAutomation = resultsElement.findElements(By.cssSelector("[role=\"option\"]"));
+		List<String> suggestionsAutomationStrings = new LinkedList<String>();
 		for (WebElement webElement : suggestionsAutomation) {
 			System.out.println("-"+webElement.getAttribute("aria-label"));
+			suggestionsAutomationStrings.add(webElement.getAttribute("aria-label"));
 		}
 		
 		Boolean coincident = true;
 		
-		for (int i=0; i<suggestionsAuto.size() && coincident ; i++) {
-			String textAuto = suggestionsAuto.get(i).getAttribute("aria-label");
-			String textAutomation = suggestionsAutomation.get(i).getAttribute("aria-label");
-			coincident = (textAuto == textAutomation);
+		String textAuto,textAutomation;
+		for (int i=0; i<suggestionsAutoStrings.size() && coincident ; i++) {
+			textAuto = suggestionsAutoStrings.get(i);
+			textAutomation = suggestionsAutomationStrings.get(i);
+			coincident = (textAuto.equals(textAutomation));
+			if(!textAuto.equals(textAutomation)) {
+				System.out.println("{"+textAuto+"  "+textAutomation+"}");
+			}
 		}
 		
 		Assert.assertFalse(coincident, "The results list are coincidents");
 		
-		resultsElement.findElements(By.className("sbct sbre")).get(0).click();
+		resultsElement.findElements(By.className("sbre")).get(0).click();
 
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[5]/div/div[12]/div[1]/div[2]/div[2]/div/div/div/div/div/div/div/div/div[1]/div/div/div[1]/div/div[3]/div[1]/div/div/div/div/div/div[1]/div/div/span/a/div/div/div/cite")));
-		WebElement pageResult = driver.findElement(By.xpath("/html/body/div[5]/div/div[12]/div[1]/div[2]/div[2]/div/div/div/div/div/div/div/div/div[1]/div/div/div[1]/div/div[3]/div[1]/div/div/div/div/div/div[1]/div/div/span/a/div/div/div/cite"));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("search")));
+		WebElement pageResult = driver.findElements(By.cssSelector("[role=\"text\"]")).get(0);
 
 		Assert.assertTrue(pageResult.getText().contains("store.steampowered.com"),"La pagina no es \"store.steampowered.com\"");
 	}
